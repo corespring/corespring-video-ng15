cf559707fe06c0064d5566628fa4349dcbfcec4b
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-12-19 16:50:57 -0500

    Add useful demo content

--- docs/demo/controllers/entry.js
+++ docs/demo/controllers/entry.js
@@ -2,3 +2,7 @@
          exports['corespring-video-ng15'] = require('corespring-video-ng15-controller');
          exports['corespring-video-ng15'].version = '../node_modules/corespring-video-ng15/controller';
          
+
+         exports['corespring-choice'] = require('corespring-choice-controller');
+         exports['corespring-choice'].version = '../node_modules/corespring-choice/controller';
+         
--- docs/demo/entry.js
+++ docs/demo/entry.js
@@ -7,19 +7,12 @@
         import Controller from 'pie-controller';
         window.pie = window.pie || {};
         window.pie.Controller = Controller;
-
-  import comp1 from 'pie-player';
-  //customElements v1 
-  customElements.define('pie-player', comp1);
-  
-
-  import comp2 from 'pie-control-panel';
-  //customElements v1 
-  customElements.define('pie-control-panel', comp2);
-  
-
-  import comp3 from 'corespring-video-ng15';
-  //customElements v1 
-  customElements.define('corespring-video-ng15', comp3);
-  
+import PiePlayer from 'pie-player';
+customElements.define('pie-player', PiePlayer);
+import PieControlPanel from 'pie-control-panel';
+customElements.define('pie-control-panel', PieControlPanel);
+import CorespringVideoNg15 from 'corespring-video-ng15';
+customElements.define('corespring-video-ng15', CorespringVideoNg15);
+import CorespringChoice from 'corespring-choice';
+customElements.define('corespring-choice', CorespringChoice);
     
